1b8532e5a98ad4346dc8d839fa69ab9bc5763cc0
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-08-15 17:35:24 +0530

    project

--- Hashan/src/components/Project.jsx
+++ Hashan/src/components/Project.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import project1 from '../assets/Project1.png'
+import project3 from '../assets/Project3.jpg'
+import project4 from '../assets/Project4.png'
 
 export default function Project() {
   return (
-    <div>
+    <div className='bg-white mx-4 mt-3 p-3 shadow-lg rounded-lg'>
+         <h1 className='font-bold text-[20px]'>Recent Project</h1>
+         <div>
+            <div className='p-6 bg-slate-300 rounded-lg my-4'>
+               <a href='https://009-shop-client.vercel.app/'><img src={project1} alt="" /></a> 
+            </div>
+            <div className='p-6 bg-slate-300 rounded-lg'>
+                
+                <a href='https://user-management-system-client-six.vercel.app/'><img src={project3} alt="" /></a> 
+            </div>
+            <div className='p-6 bg-slate-300 rounded-lg'>
+            <a href='https://book-nest-client.vercel.app/'><img src={project4} alt="" /></a> 
+            
+            </div>
+         </div>
+
       
     </div>
   )
--- Hashan/src/pages/Home.jsx
+++ Hashan/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Profiile from '../components/Profiile'
 import Skill from '../components/Skill'
+import Project from '../components/Project'
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
         <Navbar/>
         <Profiile/>
         <Skill/>
+        <Project/>
       
     </div>
   )
