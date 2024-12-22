a5279bfe63d61c77ea6c64b7c09d605572172055
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-12-22 09:45:30 +0530

    update paddign

--- Hashan/src/components/Service.jsx
+++ Hashan/src/components/Service.jsx
@@ -8,7 +8,7 @@ import {
 
 export default function Service() {
   return (
-    <div className="bg-[#0f172a]   mx-4 mt-4 p-10 shadow-lg rounded-lg">
+    <div className="bg-[#0f172a]   mx-4 mt-4 p-6 shadow-lg rounded-lg">
       <span class="relative flex h-3 w-3">
         <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
         <span class="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
